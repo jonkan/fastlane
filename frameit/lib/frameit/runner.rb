a79c4ade53229c41c349750f845f830035383338
Jonas Bromö <jonas.bromo@gmail.com>
2024-05-03 10:06:39 +0200

    [frameit] Add support for parallelization and option to skip directories

--- frameit/lib/frameit/runner.rb
+++ frameit/lib/frameit/runner.rb
@@ -1,4 +1,5 @@
 require 'fastimage'
+require 'thread'
 
 require_relative 'frame_downloader'
 require_relative 'module'
@@ -23,38 +24,74 @@ module Frameit
 
       screenshots = Dir.glob("#{path}/**/*.{png,PNG}").uniq # uniq because thanks to {png,PNG} there are duplicates
 
+      number_of_treads = 1
+      skip_directories = []
+      config_files = Dir["./**/Framefile.json"]
+      if config_files.count > 0
+        config = ConfigParser.new.load(config_files.first)
+        if config.data["number_of_treads"].to_s.length > 0
+          number_of_treads = config.data["number_of_treads"]
+        end
+
+        if config.data["skip_directories"].length > 0
+          skip_directories = config.data["skip_directories"]
+        end
+      end
+
       if screenshots.count > 0
+        queue = Queue.new
         screenshots.each do |full_path|
-          next if skip_path?(full_path)
+          next if skip_path?(full_path, skip_directories)
+          queue << full_path
+        end
 
-          begin
-            config = create_config(full_path)
-            screenshot = Screenshot.new(full_path, color, config, platform)
-
-            next if skip_up_to_date?(screenshot)
-
-            editor = editor(screenshot, config)
-
-            if editor.should_skip?
-              UI.message("Skipping framing of screenshot #{screenshot.path}.  No title provided in your Framefile.json or title.strings.")
-            else
-              Helper.show_loading_indicator("Framing screenshot '#{full_path}'")
-              editor.frame!
+        threads = []
+        number_of_treads.times do
+          threads << Thread.new do
+            while !queue.empty?
+              full_path = queue.pop(true) rescue nil
+              break unless full_path
+              process_screenshot(full_path, color, platform, number_of_treads > 1)
             end
-          rescue => ex
-            UI.error(ex.to_s)
-            UI.error("Backtrace:\n\t#{ex.backtrace.join("\n\t")}") if FastlaneCore::Globals.verbose?
           end
         end
+        threads.each(&:join)
       else
         UI.error("Could not find screenshots in current directory: '#{File.expand_path(path)}'")
       end
     end
 
-    def skip_path?(path)
+    def process_screenshot(full_path, color, platform, is_concurrent)
+      begin
+        config = create_config(full_path)
+        screenshot = Screenshot.new(full_path, color, config, platform)
+
+        return if skip_up_to_date?(screenshot)
+
+        editor = editor(screenshot, config)
+
+        if editor.should_skip?
+          UI.message("Skipping framing of screenshot #{screenshot.path}.  No title provided in your Framefile.json or title.strings.")
+        else
+          if is_concurrent
+            UI.message("Framing screenshot '#{full_path}'")
+          else
+            Helper.show_loading_indicator("Framing screenshot '#{full_path}'")
+          end
+          editor.frame!
+        end
+      rescue => ex
+        UI.error(ex.to_s)
+        UI.error("Backtrace:\n\t#{ex.backtrace.join("\n\t")}") if FastlaneCore::Globals.verbose?
+      end
+    end
+
+    def skip_path?(path, skip_directories)
       return true if path.include?("_framed.png")
       return true if path.include?(".itmsp/") # a package file, we don't want to modify that
       return true if path.include?("device_frames/") # these are the device frames the user is using
+      return true if skip_directories.any? { |dir| path.include?(dir) }
+
       device = path.rpartition('/').last.partition('-').first # extract device name
       if device.downcase.include?("watch")
         UI.error("Apple Watch screenshots are not framed: '#{path}'")
